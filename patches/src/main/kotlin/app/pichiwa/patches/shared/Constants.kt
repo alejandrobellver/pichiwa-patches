83bb08eda5ff8ce8013ac428d421bb6b7f7b8c3c
Alejandro Bellver <alejandrobellver7@gmail.com>
2026-07-12 01:07:29 +0200

    fix: use SupportedAbi.ARM64_V8A for versionCodes

--- patches/src/main/kotlin/app/pichiwa/patches/shared/Constants.kt
+++ patches/src/main/kotlin/app/pichiwa/patches/shared/Constants.kt
@@ -3,6 +3,7 @@ package app.pichiwa.patches.shared
 import app.morphe.patcher.patch.ApkFileType
 import app.morphe.patcher.patch.AppTarget
 import app.morphe.patcher.patch.Compatibility
+import app.morphe.patcher.patch.SupportedAbi
 
 object Constants {
     val WHATSAPP = Compatibility(
@@ -11,7 +12,11 @@ object Constants {
         apkFileType = ApkFileType.APK,
         appIconColor = 0x25D366,
         targets = listOf(
-            AppTarget(version = null, isExperimental = true)
+            AppTarget(
+                version = "2.26.27.4",
+                versionCodes = mapOf(SupportedAbi.ARM64_V8A to 262700402),
+                isExperimental = false
+            )
         )
     )
 }
