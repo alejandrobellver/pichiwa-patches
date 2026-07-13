85925396a35e2126e351a53e4b59c49b20afeb3c
Alejandro Bellver <alejandrobellver7@gmail.com>
2026-07-13 20:20:48 +0200

    fix(patches): use low registers for Dalvik injection in SettingsPatch

--- patches/src/main/kotlin/app/pichiwa/patches/whatsapp/SettingsPatch.kt
+++ patches/src/main/kotlin/app/pichiwa/patches/whatsapp/SettingsPatch.kt
@@ -22,32 +22,32 @@ val settingsPatch = bytecodePatch(
             parameters = listOf("Landroid/view/Menu;")
         ).let { match ->
             match.method.addInstructions(0, """
-                const-string v30, "app.pichiwa.extension.extension.PichiwaMenuHook"
-                invoke-static/range {v30 .. v30}, Ljava/lang/Class;->forName(Ljava/lang/String;)Ljava/lang/Class;
-                move-result-object v30
+                const-string v0, "app.pichiwa.extension.extension.PichiwaMenuHook"
+                invoke-static {v0}, Ljava/lang/Class;->forName(Ljava/lang/String;)Ljava/lang/Class;
+                move-result-object v0
                 
-                const-string v31, "injectMenuItems"
-                const/4 v32, 0x2
-                new-array v32, v32, [Ljava/lang/Class;
-                const/4 v33, 0x0
-                const-class v34, Landroid/app/Activity;
-                aput-object v34, v32, v33
-                const/4 v33, 0x1
-                const-class v34, Landroid/view/Menu;
-                aput-object v34, v32, v33
+                const-string v1, "injectMenuItems"
+                const/4 v2, 0x2
+                new-array v2, v2, [Ljava/lang/Class;
+                const/4 v3, 0x0
+                const-class v4, Landroid/app/Activity;
+                aput-object v4, v2, v3
+                const/4 v3, 0x1
+                const-class v4, Landroid/view/Menu;
+                aput-object v4, v2, v3
                 
-                invoke-virtual/range {v30 .. v32}, Ljava/lang/Class;->getMethod(Ljava/lang/String;[Ljava/lang/Class;)Ljava/lang/reflect/Method;
-                move-result-object v30
+                invoke-virtual {v0, v1, v2}, Ljava/lang/Class;->getMethod(Ljava/lang/String;[Ljava/lang/Class;)Ljava/lang/reflect/Method;
+                move-result-object v0
                 
-                const/4 v31, 0x0
-                const/4 v32, 0x2
-                new-array v32, v32, [Ljava/lang/Object;
-                const/4 v33, 0x0
-                aput-object p0, v32, v33
-                const/4 v33, 0x1
-                aput-object p1, v32, v33
+                const/4 v1, 0x0
+                const/4 v2, 0x2
+                new-array v2, v2, [Ljava/lang/Object;
+                const/4 v3, 0x0
+                aput-object p0, v2, v3
+                const/4 v3, 0x1
+                aput-object p1, v2, v3
                 
-                invoke-virtual/range {v30 .. v32}, Ljava/lang/reflect/Method;->invoke(Ljava/lang/Object;[Ljava/lang/Object;)Ljava/lang/Object;
+                invoke-virtual {v0, v1, v2}, Ljava/lang/reflect/Method;->invoke(Ljava/lang/Object;[Ljava/lang/Object;)Ljava/lang/Object;
             """)
         }
     }
