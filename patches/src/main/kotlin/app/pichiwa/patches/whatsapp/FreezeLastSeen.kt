ea15984d6c1f841eae32af01443525d539e77227
Alejandro Bellver <alejandrobellver7@gmail.com>
2026-07-13 06:00:46 +0200

    Fix ClassNotFoundException by reading SharedPreferences locally in early patches

--- patches/src/main/kotlin/app/pichiwa/patches/whatsapp/FreezeLastSeen.kt
+++ patches/src/main/kotlin/app/pichiwa/patches/whatsapp/FreezeLastSeen.kt
@@ -6,12 +6,10 @@ import app.morphe.patcher.patch.bytecodePatch
 import app.morphe.patcher.string
 import app.pichiwa.patches.shared.Constants.WHATSAPP
 
-private const val EXT = "Lapp/pichiwa/extension/extension/WExtension;"
-
 @Suppress("unused")
 val freezeLastSeen = bytecodePatch(
     name = "Freeze Last Seen",
-    description = "Congela la hora de última conexión.",
+    description = "Congela la hora de ultima conexion.",
     default = false
 ) {
     compatibleWith(WHATSAPP)
@@ -21,35 +19,21 @@ val freezeLastSeen = bytecodePatch(
             filters = listOf(string("presencestatemanager/setAvailable/new-state: "))
         ).let { match ->
             match.method.addInstructions(0, """
-                .catch Ljava/lang/Throwable; {:try_start_freeze .. :try_end_freeze} :catch_freeze
-                :try_start_freeze
-                const-string v0, "app.pichiwa.extension.extension.WExtension"
-                invoke-static {v0}, Ljava/lang/Class;->forName(Ljava/lang/String;)Ljava/lang/Class;
+                invoke-static {}, LX/00I;->A00()Landroid/app/Application;
                 move-result-object v0
-                
-                const-string v1, "shouldFreezeLastSeen"
+                if-nez v0, :cond_skip_pichiwa
+                const-string v1, "pichiwa_prefs"
                 const/4 v2, 0x0
-                new-array v2, v2, [Ljava/lang/Class;
-                invoke-virtual {v0, v1, v2}, Ljava/lang/Class;->getMethod(Ljava/lang/String;[Ljava/lang/Class;)Ljava/lang/reflect/Method;
+                invoke-virtual {v0, v1, v2}, Landroid/content/Context;->getSharedPreferences(Ljava/lang/String;I)Landroid/content/SharedPreferences;
                 move-result-object v0
-                
-                const/4 v1, 0x0
-                new-array v2, v1, [Ljava/lang/Object;
-                invoke-virtual {v0, v1, v2}, Ljava/lang/reflect/Method;->invoke(Ljava/lang/Object;[Ljava/lang/Object;)Ljava/lang/Object;
-                move-result-object v0
-                
-                check-cast v0, Ljava/lang/Boolean;
-                invoke-virtual {v0}, Ljava/lang/Boolean;->booleanValue()Z
+                if-nez v0, :cond_skip_pichiwa
+                const-string v1, "freeze_last_seen"
+                invoke-interface {v0, v1, v2}, Landroid/content/SharedPreferences;->getBoolean(Ljava/lang/String;Z)Z
                 move-result v0
-                if-nez v0, :original
+                if-nez v0, :cond_skip_pichiwa
                 return-void
-                :try_end_freeze
-                goto :original
-                
-                :catch_freeze
-                move-exception v0
-                :original
-            """)
+                :cond_skip_pichiwa
+            """.trimIndent())
         }
     }
 }
