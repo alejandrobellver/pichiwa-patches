4ae581f7f516e8cbb769c1b96ae130b4c30c5c6d
Alejandro Bellver <alejandrobellver7@gmail.com>
2026-07-15 01:24:44 +0200

    fix: dynamically hook LX/5k7 interface and intercept CM2 to force view once state to unread in memory

--- patches/src/main/kotlin/app/pichiwa/patches/whatsapp/AntiViewOnce.kt
+++ patches/src/main/kotlin/app/pichiwa/patches/whatsapp/AntiViewOnce.kt
@@ -19,6 +19,9 @@ val antiViewOnce = bytecodePatch(
     compatibleWith(WHATSAPP)
 
     execute {
+        var viewOnceInterface: String? = null
+        var setViewOnceStateMethod: String? = null
+
         Fingerprint(
             filters = listOf(string("GET_VIEW_ONCE_STATE_BY_MESSAGE_ROW_ID_SQL"))
         ).let { match ->
@@ -26,6 +29,10 @@ val antiViewOnce = bytecodePatch(
             val instructions = impl.instructions.toList()
             
             val lastInvoke = instructions.last { it.opcode.name == "invoke-interface" }
+            val methodRef = (lastInvoke as com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction).reference as com.android.tools.smali.dexlib2.iface.reference.MethodReference
+            viewOnceInterface = methodRef.definingClass
+            setViewOnceStateMethod = methodRef.name
+            
             val regD = (lastInvoke as com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction).registerD
             val index = instructions.indexOf(lastInvoke)
             
@@ -40,6 +47,18 @@ val antiViewOnce = bytecodePatch(
             match.method.addInstructions(0, """
                 const/4 p1, 0x0
             """)
+        }
+
+        classDefForEach { def ->
+            if (viewOnceInterface != null && def.interfaces.contains(viewOnceInterface)) {
+                val method = def.methods.firstOrNull { it.name == setViewOnceStateMethod }
+                if (method != null) {
+                    val mutableMethod = mutableClassDefBy(def).methods.first { it.name == method.name && it.returnType == method.returnType }
+                    mutableMethod.addInstructions(0, """
+                        const/4 p1, 0x0
+                    """)
+                }
+            }
         }
 
         classDefForEach { def ->
