7f5403d956e2c97f988fba5101bcee094f34587a
Alejandro Bellver <alejandrobellver7@gmail.com>
2026-07-15 08:49:32 +0200

    fix(whatsapp): spoof Java PackageInfo.signatures and restore MicroG-RE Play Integrity hook

--- patches/src/main/kotlin/app/pichiwa/patches/whatsapp/LoginFix.kt
+++ patches/src/main/kotlin/app/pichiwa/patches/whatsapp/LoginFix.kt
@@ -58,6 +58,54 @@ val loginFix = bytecodePatch(
         }
 
 
+        
+        // --- 3. Spoof Java Signatures (PackageInfo.signatures) ---
+        classDefForEach { def ->
+            def.methods.forEach { method ->
+                val impl = method.implementation ?: return@forEach
+                val matches = impl.instructions.mapIndexedNotNull { index, instr ->
+                    val ref = (instr as? ReferenceInstruction)?.reference?.toString()
+                    if (ref == "Landroid/content/pm/PackageManager;->getPackageInfo(Ljava/lang/String;I)Landroid/content/pm/PackageInfo;" && instr.opcode.name.startsWith("invoke-virtual")) {
+                        index
+                    } else null
+                }
+                
+                if (matches.isNotEmpty()) {
+                    val mutableMethod = mutableClassDefBy(def).methods.first { it.name == method.name && it.parameters == method.parameters && it.returnType == method.returnType }
+                    matches.reversed().forEach { invokeIdx ->
+                        val invokeInstr = impl.instructions.elementAt(invokeIdx)
+                        val vC = if (invokeInstr is com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction) invokeInstr.registerC else (invokeInstr as com.android.tools.smali.dexlib2.iface.instruction.RegisterRangeInstruction).startRegister
+                        val vD = if (invokeInstr is com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction) invokeInstr.registerD else vC + 1
+                        val vE = if (invokeInstr is com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction) invokeInstr.registerE else vC + 2
+                        
+                        val moveResultInstr = impl.instructions.elementAtOrNull(invokeIdx + 1) as? OneRegisterInstruction ?: return@forEach
+                        val vResult = moveResultInstr.registerA
+                        
+                        mutableMethod.addInstructions(invokeIdx + 2, """
+                            if-nez v$vResult, :skip_spoof_$invokeIdx
+                            iget-object v$vD, v$vResult, Landroid/content/pm/PackageInfo;->signatures:[Landroid/content/pm/Signature;
+                            if-nez v$vD, :skip_spoof_$invokeIdx
+                            
+                            const-string v$vD, "MIIDMjCCAvCgAwIBAgIETCU2pDALBgcqhkjOOAQDBQAwfDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExFDASBgNVBAcTC1NhbnRhIENsYXJhMRYwFAYDVQQKEw1XaGF0c0FwcCBJbmMuMRQwEgYDVQQLEwtFbmdpbmVlcmluZzEUMBIGA1UEAxMLQnJpYW4gQWN0b24wHhcNMTAwNjI1MjMwNzE2WhcNNDQwMjE1MjMwNzE2WjB8MQswCQYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEUMBIGA1UEBxMLU2FudGEgQ2xhcmExFjAUBgNVBAoTDVdoYXRzQXBwIEluYy4xFDASBgNVBAsTC0VuZ2luZWVyaW5nMRQwEgYDVQQDEwtCcmlhbiBBY3RvbjCCAbgwggEsBgcqhkjOOAQBMIIBHwKBgQD9f1OBHXUSKVLfSpwu7OTn9hG3UjzvRADDHj+AtlEmaUVdQCJR+1k9jVj6v8X1ujD2y5tVbNeBO4AdNG/yZmC3a5lQpaSfn+gEexAiwk+7qdf+t8Yb+DtX58aophUPBPuD9tPFHsMCNVQTWhaRMvZ1864rYdcq7/IiAxmd0UgBxwIVAJdgUI8VIwvMspK5gqLrhAvwWBz1AoGBAPfhoIXWmz3ey7yrXDa4V7l5lK+7+jrqgvlXTAs9B4JnUVlXjrrUWU/mcQcQgYC0SRZxI+hMKBYTt88JMozIpuE8FnqLVHyNKOCjrh4rs6Z1kW6jfwv6ITVi8ftiegEkO8yk8b6oUZCJqIPf4VrlnwaSi2ZegHtVJWQBTDv+z0kqA4GFAAKBgQDRGYtLgWh7zyRtQainJfCpiaUbzjJuhMgo4fVWZIvXHaSHBU1t5w//S0lDK2hiqkj8KpMWGywVov9eZxZy37V26dEqr/c2m5qZ0E+ynSu7sqUD7kGx/zeIcGT0H+KAVgkGNQCo5Uc0koLRWYHNtYoIvt5R3X6YZylbPftF/8ayWTALBgcqhkjOOAQDBQADLwAwLAIUAKYCp0d6z4QQdyN74JDfQ2WCyi8CFDUM4CaNB+ceVXdKtOrNTQcc0e+t"
+                            const/4 v$vE, 0x0
+                            invoke-static {v$vD, v$vE}, Landroid/util/Base64;->decode(Ljava/lang/String;I)[B
+                            move-result-object v$vD
+                            
+                            new-instance v$vC, Landroid/content/pm/Signature;
+                            invoke-direct {v$vC, v$vD}, Landroid/content/pm/Signature;-><init>([B)V
+                            
+                            iget-object v$vD, v$vResult, Landroid/content/pm/PackageInfo;->signatures:[Landroid/content/pm/Signature;
+                            const/4 v$vE, 0x0
+                            aput-object v$vC, v$vD, v$vE
+                            
+                            :skip_spoof_$invokeIdx
+                        """)
+                    }
+                }
+            }
+        }
+
+
         // --- 4. Global MicroG-RE Package Redirection ---
         classDefForEach { def ->
             def.methods.forEach { method ->
@@ -65,7 +113,7 @@ val loginFix = bytecodePatch(
                 val matches = impl.instructions.mapIndexedNotNull { index, instr ->
                     if (instr is ReferenceInstruction && instr.reference is StringReference) {
                         val str = (instr.reference as StringReference).string
-                        if (str == "com.android.vending") index to "com.whatsapp.dummy"
+                        if (str == "com.android.vending") index to "app.revanced.android.vending"
                         else null
                     } else null
                 }
