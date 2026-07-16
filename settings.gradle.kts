8909ec03171e42be44155a41fb79d728e987bc7c
Alejandro Bellver <alejandrobellver7@gmail.com>
2026-07-16 08:10:08 +0200

    fix: restore MorpheApp maven registry

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,14 @@ pluginManagement {
         mavenLocal()
         gradlePluginPortal()
         google()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/MorpheApp/registry")
+            credentials {
+                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
+                password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
         
         maven { url = uri("https://jitpack.io") }
     }
